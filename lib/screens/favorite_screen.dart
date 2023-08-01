a89ff08d5807a6b5f3ca17f8fd87a4b14ad09c03
sa-l-aash <salaashkaelo@gmail.com>
2023-08-01 21:05:51 +0300

    added bottom navigation bar

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:playground/screens/home_screen.dart';
+import 'package:playground/screens/search_screen.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -10,6 +13,64 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('These are your favorites pictures',
+              style: GoogleFonts.alkatra()
+                  .copyWith(fontSize: 28, fontWeight: FontWeight.w700)),
+        ),
+        body: const SingleChildScrollView(
+            padding: EdgeInsets.all(40), child: Column(children: <Widget>[])),
+        bottomNavigationBar: SizedBox(
+          height: 78,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.black,
+            type: BottomNavigationBarType.fixed,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    },
+                    icon: const Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const SearchScreen()));
+                    },
+                    icon: const Icon(Icons.search),
+                    color: Colors.grey,
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    },
+                    icon: const Icon(Icons.favorite),
+                    color: Colors.grey,
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    },
+                    icon: const Icon(Icons.account_box),
+                    color: Colors.grey,
+                  ),
+                  label: ''),
+            ],
+          ),
+        ));
   }
 }
