e41a1de88722ce25e142f6bc4c1b268d7bae1ea7
sa-l-aash <salaashkaelo@gmail.com>
2023-08-08 19:28:01 +0300

    added genre

--- lib/models/picture_model.dart
+++ lib/models/picture_model.dart
@@ -1,14 +1,12 @@
-//this is the model of a picture
-
 class Picture {
-  final String pictureName;
-  final int yearTaken;
+  final String name;
+  final String genre;
+  final String yearTaken;
   final String imageUrl;
-// Constructor for the 'Picture' class.
-  // When creating a new 'Picture' object, this constructor must be used and all required properties must be provided.
-  Picture({
-    required this.pictureName,
-    required this.yearTaken,
-    required this.imageUrl,
-  });
+
+  Picture(
+      {required this.name,
+      required this.genre,
+      required this.yearTaken,
+      required this.imageUrl});
 }
